ca8aabfdacefdb83e6486b5ef935c64b6e48fdda
allenly3 <30699006+allenly3@users.noreply.github.com>
2024-02-01 00:39:50 -0500

    Update sendCol.c

--- lesson4/sendCol.c
+++ lesson4/sendCol.c
@@ -1,10 +1,12 @@
 #include "stdio.h"
 #include "mpi.h"
+#define n 10
 
 int main(int argc, char* argv[]) {
     int p;
     int my_rank;
-    float A[10][10];
+    float A[n][n];
+    float T[n][n];
     MPI_Status status;
     MPI_Datatype column_mpi_t;
     int i, j;
@@ -19,16 +21,22 @@ int main(int argc, char* argv[]) {
         for (i = 0; i < 10; i++)
             for (j = 0; j < 10; j++)
                 A[i][j] = (float) j;
-        MPI_Send(&(A[0][2]), 1, column_mpi_t, 1, 0,
-            MPI_COMM_WORLD);
-    } else { /* my_rank = 1 */
-        MPI_Recv(&(A[0][2]), 1, column_mpi_t, 0, 0,
-            MPI_COMM_WORLD, &status);
-        for (i = 0; i < 10; i++)
-            printf("%3.1f ", A[i][2]);
+        MPI_Send(&(A[0][2]), 1, column_mpi_t, 1, 0,MPI_COMM_WORLD);
+    } else { 
+        for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++)
+                T[i][j] = 0.0;
+
+        MPI_Recv(&(T[0][2]), 1, column_mpi_t, 0, 0,MPI_COMM_WORLD, &status);
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++)
+                printf("%4.1f ", T[i][j]);
+            printf("\n");
+        }
         printf("\n");
     }
 
     MPI_Finalize();
+    return 0 ; 
 }  /* main */
 	
